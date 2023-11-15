495afc2b8372e21cabd22f9755bc9f1ed2260ed2
LinseNain <141765188+LinseNain@users.noreply.github.com>
2023-11-15 20:44:25 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generatorExcuse();
+  });
+  console.log("nain");
 };
+let generatorExcuse = () => {
+  let who = ["Mi pájaro", "Mi zebra ", "Mi avion"];
+  let acction = [
+    "esta volando ",
+    "esta jugando ",
+    "esta votando ",
+    "esta cortandose el pelo"
+  ];
+  let where = ["en la casa del presidente", "en la carretera m-50", "en sol"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let acctionIndex = Math.floor(Math.random() * acction.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return who[whoIndex] + " " + acction[acctionIndex] + " " + where[whereIndex];
+};
+//write your code here
